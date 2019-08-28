ea006de954e52cd2bc256977f1f43d8aa398a227
Sumit <sumit.r.agr@gmail.com>
2019-08-28 03:20:55 -0400

    Changed main function logic, added TinyDB use

--- bot.py
+++ bot.py
@@ -15,6 +15,10 @@ import time
 # Random library
 import random
 
+# TinyDB library to store data in a JSON file
+from tinydb import TinyDB, Query
+db = TinyDB('db.json')
+
 # Adding twitter developer credentials
 auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
 auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
@@ -31,27 +35,23 @@ def bot_login():
                 client_secret = config.REDDIT_CLIENT_SECRET,
                 user_agent = config.REDDIT_USERAGENT)
     
-    print(f"Successfully logged in the bot as {bot_login_info.user.me()}")
+    print(f"Logged into Reddit as {bot_login_info.user.me()}")
     
     return bot_login_info
-
-# Txt file to store random tunes that the bot has tweeted
-def get_randomtune_list():
-    with open("posted_tunes.txt", "r") as file:
-        posted_tunes = file.read()
-        posted_tunes = posted_tunes.split("\n")
-    
-    return posted_tunes
-
-# Initializing a variable
-random_tune_file = get_randomtune_list()
 
 # Main function to get the random tune from reddit submissions and then filtering youtube URLs. 
 # Also checks if the tune has been posted and if it has, randomizes the list until new random tune 
 def get_random_tune(bot_login_info):
+    '''
+    The function takes top submissions from the specificed subreddits and then filters them for youtube links only.
+    After appending it to the created list, the list is randomized into a lambda function.
+    It then checks with the json file if it has been used, if it is used, it loops until a new non-used tune is found.
+    After getting the tune, the tune is posted to Twitter.
+    '''
+
     list_of_submissions = []
     
-    for submission in bot_login_info.subreddit('listentothis+music').hot(limit = 25):
+    for submission in bot_login_info.subreddit('listentothis+music+hiphopheads').hot(limit = 50):
         if 'youtube.com/' in submission.url:
             list_of_submissions.append(submission.url)
         elif 'youtu.be/' in submission.url:
@@ -59,26 +59,30 @@ def get_random_tune(bot_login_info):
         else:
             pass
     
-    tune_randomizer = random.choice(list_of_submissions)
+    tune_randomizer = lambda: random.choice(list_of_submissions)
     
-    with open("posted_tunes.txt", "a") as file:
-        file.write(tune_randomizer + "\n")
+    never_used = False
+    while not never_used:
+        tune_randomizer_variable = tune_randomizer()
+        if db.contains(Query()['tune'] == tune_randomizer_variable) == False:
 
-    if tune_randomizer in random_tune_file:
-        new_random_tune = random.choice(list_of_submissions)
-        
-        with open("posted_tunes.txt", "a") as file:
-            file.write(new_random_tune + "\n")
-        
-        print(time.strftime("%c"))
-        print(f"Posting to Twitter - {new_random_tune}")
-        api.update_status(f"{time.strftime('Here is your random tune for %A! The current time is %X')} \n {new_random_tune}")
-    else:
-        print(time.strftime("%c"))
-        print(f"Posting to Twitter - {tune_randomizer}")
-        api.update_status(f"{time.strftime('Here is your random tune for %A! The current time is %X')} \n {tune_randomizer}")
+            db.insert({'tune': tune_randomizer_variable})
+
+            print(time.strftime("%c"))
+            print(f"Posting to Twitter - {tune_randomizer_variable}")
+            
+            api.update_status(f"{time.strftime('Here is your random tune for %A! The current time is %X')} \n {tune_randomizer_variable}")
+
+            never_used = True
+
+            if never_used:
+                continue
+
+        else:
+            pass
 
 # While loop to continuously run the function every 4 hours
 while (True):
     get_random_tune(bot_login())
+    print("Sleeping for 4 hours!")
     time.sleep(14400)
